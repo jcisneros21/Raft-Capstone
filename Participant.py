cb186aacdc6a6535d59c52f380ccfde7c1ea2b12
jcisneros21 <jcisneros@slu.edu>
2016-11-22 12:47:35 -0600

    Removed __pycache__

--- Participant.py
+++ Participant.py
@@ -14,6 +14,7 @@ class Participant:
 
 class CandidateNode:
   self __init__(self):
+    Participant.__init__(self)
     self.electionTimeout
     self.votes
     self.timer
@@ -21,7 +22,8 @@ class CandidateNode:
   def requestVotes():
       for server in self.nodeaddrs:
          message = RequestVote_pb2.RequestVote()
-         message.from = self.addr
+         # How are we going to do this?
+         # message.from = self.addr
          OutQueue.put_nowait(RequestVote_pb2)
 
   def selectTimeout():
@@ -33,3 +35,21 @@ class CandidateNode:
 
   def initTimer():
       self.timer = threading.Timer(self.electionTimeout, ).start()
+
+class FollowerNode():
+    self __init__(sef):
+        Participant.__init__(self)
+        self.electionTimeout
+
+    def selectTimeout():
+        self.electionTimeout = random.uniform(0.150,0.300)
+
+    def appendEnrtyReply():
+
+
+    def requestVoteReply():
+
+    def transToCandidate():
+
+    def initTimer():
+        self.timer = threading.Timer(self.electionTimeout, ).start()
