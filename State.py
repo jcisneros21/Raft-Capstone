64a8314c07aee5dc620702ab6640a46d5274df57
jcisneros21 <jcisneros@slu.edu>
2016-11-27 14:43:33 -0600

    Adding State class

---
+++ State.py
@@ -0,0 +1,22 @@
+class LeaderState():
+  def __init__(self):
+    print("Leader")
+  def appendEntries():
+
+class CandidateState():
+  def __init__(self):
+    print("Candidate")
+    self.votes
+
+  def requestVotes():
+      for server in self.nodeaddrs:
+         message = RequestVote_pb2.RequestVote()
+         OutQueue.put_nowait(RequestVote_pb2)
+
+class FollowerState():
+    def __init__(self):
+      print("Follower")
+
+    def appendEnrtyReply():
+
+    def requestVoteReply():
